229b16d367f5526db8aebb971d9f057781417c12
Florian Joseph <flocamj@gmail.com>
2022-11-01 21:50:44 +0100

    changer la taille des flocons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1400,
+  height: 855,
   physics: {
     default: "arcade",
     arcade: {
@@ -17,7 +17,7 @@ var config = {
 };
 
 var player;
-var stars;
+var items;
 var bombs;
 var platforms;
 var cursors;
@@ -30,7 +30,8 @@ var game = new Phaser.Game(config);
 function preload() {
   this.load.image("sky", "./assets/img/sky.png");
   this.load.image("ground", "./assets/img/platform.png");
-  this.load.image("star", "./assets/img/ring.png")
+  this.load.image("ground2", "./assets/img/platform2.png");
+  this.load.image("item", "./assets/img/item.png");
   this.load.image("bomb", "./assets/img/bomb.png");
   this.load.spritesheet("dude", "./assets/img/dude.png", {
     frameWidth: 32,
@@ -44,15 +45,17 @@ function create() {
 
   //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = this.physics.add.staticGroup();
+  platforms2 = this.physics.add.staticGroup();
 
   //  Here we create the ground.
   //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+  platforms.create(700, 1100, "ground").setScale(2.5).refreshBody();
+  platforms2.create(700, 550, "ground2").setScale(3);
 
   //  Now let's create some ledges
-  platforms.create(600, 400, "ground");
-  platforms.create(50, 250, "ground");
-  platforms.create(750, 220, "ground");
+  platforms.create(1080, 800, "ground");
+
+  platforms2.create(1080, 590, "ground2");
 
   // The player and its settings
   player = this.physics.add.sprite(100, 450, "dude");
@@ -86,13 +89,13 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
 
   //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-  stars = this.physics.add.group({
-    key: "star",
+  items = this.physics.add.group({
+    key: "item",
     repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 },
+    setXY: { x: 25, y: 0, stepX: 120 },
   });
 
-  stars.children.iterate(function (child) {
+  items.children.iterate(function (child) {
     //  Give each star a slightly different bounce
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
   });
@@ -107,11 +110,11 @@ function create() {
 
   //  Collide the player and the stars with the platforms
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(items, platforms);
   this.physics.add.collider(bombs, platforms);
 
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player, items, collectItems, null, this);
 
   this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
@@ -140,16 +143,16 @@ function update() {
   }
 }
 
-function collectStar(player, star) {
-  star.disableBody(true, true);
+function collectItems(player, item) {
+  item.disableBody(true, true);
 
   //  Add and update the score
   score += 10;
   scoreText.setText("Score: " + score);
 
-  if (stars.countActive(true) === 0) {
+  if (items.countActive(true) === 0) {
     //  A new batch of stars to collect
-    stars.children.iterate(function (child) {
+    items.children.iterate(function (child) {
       child.enableBody(true, child.x, 0, true, true);
     });
 
