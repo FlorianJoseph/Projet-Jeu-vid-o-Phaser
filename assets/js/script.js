ce24551dd4f97d320347cdb1739c7b20df76511c
Your Name <flocamj@gmail.com>
2022-11-03 16:25:22 +0100

    créer les plateformes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var config = {
   type: Phaser.AUTO,
-  width: 1400,
-  height: 855,
+  width: 1200,
+  height: 900,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 800 },
       debug: false,
     },
   },
@@ -20,87 +20,100 @@ var player;
 var items;
 var bombs;
 var platforms;
+var plate;
 var cursors;
-var score = 0;
-var gameOver = false;
-var scoreText;
 var ring;
+var score = 0;
+var scoreText;
+var dropItem;
+var gameOver = false;
 
 var game = new Phaser.Game(config);
 
 // précharge les éléments
-
 function preload() {
   this.load.image("sky", "./assets/img/sky.png");
   this.load.image("plateforme", "./assets/img/plateforme.png");
+  this.load.image("plateforme-sol", "./assets/img/plateforme-invisible.png");
   this.load.spritesheet("item", "./assets/img/ring-sprite.png", {
     frameWidth: 115,
     frameHeight: 115,
   });
   this.load.image("bomb", "./assets/img/bomb.png");
-  this.load.spritesheet("dude", "./assets/img/sonic.png", {
-    frameWidth: 102,
-    frameHeight: 122,
+  this.load.spritesheet("perso", "./assets/img/perso.png", {
+    frameWidth: 95,
+    frameHeight: 95,
   });
-  this.load.spritesheet("dude2", "./assets/img/sonic2.png", {
-    frameWidth: 102,
-    frameHeight: 122,
-  });
-
   this.load.audio("dropItem", "./assets/sound/soundRing.mp3");
 }
 
 // crée les les éléments à afficher
-
 function create() {
   //  ajoute le fond
-  this.add.image(400, 300, "sky");
+  this.add.image(600, 450, "sky");
 
   //  crée le sol
   plateforme = this.physics.add.staticGroup();
-  plateforme.create(700, 950, "plateforme").setScale(1.8).refreshBody();
+  plateforme.create(600, 1160, "plateforme-sol").setScale(1.8).refreshBody();
+
+  // crée les plateformes
+  // plate = this.physics.add.staticGroup();
+  // plate.create(400, 500, "plateforme");
 
   // ajoute le joueur et ses propriétés
-  player = this.physics.add.sprite(100, 450, "dude").setScale(1.2);
+  player = this.physics.add.sprite(100, 450, "perso").setScale(1.5);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
-  //  animations du personnage avec le sprite
+  // animations du personnage avec le sprite
+  // animation vers la gauche
   this.anims.create({
     key: "left",
-    frames: this.anims.generateFrameNumbers("dude2", { start: 0, end: 9 }),
+    frames: this.anims.generateFrameNumbers("perso", { start: 25, end: 30 }),
     frameRate: 10,
     repeat: -1,
   });
 
+  // animation à l'arrêt
   this.anims.create({
     key: "turn",
-    frames: [{ key: "dude", frame: 0 }],
+    frames: [{ key: "perso", frame: 0 }],
     frameRate: 20,
   });
 
+  // animation vers la droite
   this.anims.create({
     key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 13 }),
+    frames: this.anims.generateFrameNumbers("perso", { start: 17, end: 22 }),
     frameRate: 10,
     repeat: -1,
+  });
+
+  // animation de la mort
+  this.anims.create({
+    key: "dead",
+    frames: [{ key: "perso", frame: 7 }],
+    frameRate: 20,
   });
 
   //  evemement clavier
   cursors = this.input.keyboard.createCursorKeys();
 
   //  crée les items et ses propriétés
-
   items = this.physics.add.group({
     key: "item",
     repeat: 8,
-    bounceX: Phaser.Math.FloatBetween(0.4, 0.8),
-    bounceY: Phaser.Math.FloatBetween(0.4, 0.8),
-    velocityX: 50,
-    velocityY: 120,
+    bounceX: Phaser.Math.FloatBetween(0.2, 0.4),
+    bounceY: Phaser.Math.FloatBetween(0.2, 0.8),
+    velocityX: Phaser.Math.Between(-50, 50),
+    velocityY: 50,
   });
 
-  Phaser.Actions.RandomRectangle(items.getChildren(), this.cameras.main);
+  // crée le retangle d'apparition des items
+  const rect = new Phaser.Geom.Rectangle(0, 0, 1500, 600);
+
+  // apparition des items dans le rectangle
+  Phaser.Actions.RandomRectangle(items.getChildren(), rect);
 
   items.children.iterate(function (child) {
     child.setCollideWorldBounds(true);
@@ -108,7 +121,6 @@ function create() {
   });
 
   // crée l'animation des items
-
   this.anims.create({
     key: "itemAnim",
     frames: this.anims.generateFrameNumbers("item", { start: 0, end: 9 }),
@@ -116,112 +128,111 @@ function create() {
   });
 
   // crée la physique des bombes
-
   bombs = this.physics.add.group();
 
   //  Affiche le score avec un item au bout
-  scoreText = this.add.text(16, 16, "score: 0", {
-    fontSize: "32px",
-    fill: "#000",
+  scoreText = this.add.text(16, 16, "Score:0", {
+    fontSize: "40px",
+    fill: "aliceblue",
   });
 
-  item = this.add.sprite(195, 30, "item").setScale(0.3);
+  item = this.add.sprite(260, 33, "item").setScale(0.3);
 
   // crée les collisions
-
-  this.physics.add.collider(player, plateforme);
   this.physics.add.collider(items, plateforme);
+  this.physics.add.collider(items, plate);
+  this.physics.add.collider(player, plateforme);
+  this.physics.add.collider(player, plate);
   this.physics.add.collider(bombs, plateforme);
-  this.physics.add.overlap(player, items, collectItems, null, this);
+  this.physics.add.collider(bombs, plate);
   this.physics.add.collider(player, bombs, hitBomb, null, this);
+  this.physics.add.overlap(player, items, collectItems, null, this);
+
+  // crée la musique au ramassage
+  dropItem = this.sound.add("dropItem");
 }
 
 // crée les événements
-
 function update() {
   if (gameOver) {
     return;
   }
 
   // anime le personnage avec les déplacements vers le bas
-
   if (cursors.left.isDown) {
-    player.setVelocityX(-160);
+    player.setVelocityX(-300);
 
     // anime le personnage avec les déplacements vers la gauche
-
     player.anims.play("left", true);
   } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
+    player.setVelocityX(300);
 
     // anime le personnage avec les déplacements vers la droite
-
     player.anims.play("right", true);
   } else {
     player.setVelocityX(0);
 
     // image du personnage qui ne bouge pas
-
     player.anims.play("turn");
   }
 
   if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+    player.setVelocityY(-500);
   }
 
   // Anime les items
-
   items.children.iterate(function (child) {
     child.anims.play("itemAnim", true);
   });
 
   // Anime l'item après le score
-
   item.anims.play("itemAnim", true);
 }
 
 // fonction qui collecte les items
-
 function collectItems(player, item) {
   item.disableBody(true, true);
+  // joue la musique de ramassage
+  dropItem.play();
 
   // monte le score et l'affiche
-
   score += 1;
-  scoreText.setText("Score: " + score);
+  scoreText.setText("Score:" + score);
 
   if (items.countActive(true) === 0) {
     // faire disparaitre les items au ramassage
-
     items.children.iterate(function (child) {
       child.enableBody(true, child.x, 0, true, true);
     });
 
     // fait apparaitre aléatoirement une bombe si il reste 0 items
-
     var x =
       player.x < 400
         ? Phaser.Math.Between(400, 800)
         : Phaser.Math.Between(0, 400);
 
     // définit les propriétés de la bombe
-
     var bomb = bombs.create(x, 16, "bomb");
     bomb.setBounce(1);
     bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.setVelocity(Phaser.Math.Between(-100, 100), 20);
+    bomb.setScale(0.05);
     bomb.allowGravity = false;
   }
 }
 
 // fonction qui fait perdre quand on touche une bombe
-
 function hitBomb(player, bomb) {
   this.physics.pause();
 
   player.setTint(0xff0000);
 
-  player.anims.play("turn");
+  player.anims.play("dead");
+
+  gameOverText = this.add.text(320, 400, "Game Over", {
+    fontSize: "100px",
+    fill: "aliceblue",
+  });
 
   gameOver = true;
 }
